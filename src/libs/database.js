bfc957eda90c1f5282e7282692be85a40b6c06ef
1555921 <60400737+1555921@users.noreply.github.com>
2022-12-16 16:24:53 -0500

    Update database.js

--- src/libs/database.js
+++ src/libs/database.js
@@ -4,13 +4,13 @@ import chalk from 'chalk';
 export default async () => {
 
     const url = process.env.DATABASE;
-    console.log(chalk.green(`[MONGO] - Establish new connection with url: ${url}`));
+    console.log(chalk.green(`[MONGO] - Establish new connection with database`));
 
     try {
         await mongoose.connect(url);
-        console.log(chalk.green(`[MONGO] - Connected to: ${url}`)); 
+        console.log(chalk.green(`[MONGO] - Connected to database`)); 
     } catch(err) {
-        console.log(chalk.red(`[MONGO] - Cannot connect to: ${url}\n ${err} ... \n Exiting`));
+        console.log(chalk.red(`[MONGO] - Cannot connect to database Exiting`));
         process.exit(1);
     }
     
